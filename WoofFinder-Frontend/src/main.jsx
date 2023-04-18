b6803f5e3f3411be66598e88907984dca8d87a13
Guillem Albo Pintor <albopintor@gmail.com>
2023-04-18 15:51:54 +0200

    React wireframed project

--- WoofFinder-Frontend/src/main.jsx
+++ WoofFinder-Frontend/src/main.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import home from './home'
-import './index.css'
+import './assets/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route exact path='/' Component={App}/>
-        <Route exact path='/home' Component={home}/>
-      </Switch>
-    </Router>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </React.StrictMode>
 )
