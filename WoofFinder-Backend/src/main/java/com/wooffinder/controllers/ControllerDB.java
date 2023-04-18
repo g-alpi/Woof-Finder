0fab94f1dcfc19e8a356c2503c3922d1871c4a62
Guillem Albo Pintor <albopintor@gmail.com>
2023-04-18 17:32:17 +0200

    Controller fix

--- WoofFinder-Backend/src/main/java/com/wooffinder/controllers/ControllerDB.java
+++ WoofFinder-Backend/src/main/java/com/wooffinder/controllers/ControllerDB.java
@@ -1,26 +1,14 @@
 package com.wooffinder.controllers;
 
 
-import java.util.*;
-
-
+import org.json.JSONObject;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.web.bind.annotation.*;
-
-import org.json.JSONObject;
-
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.context.annotation.Bean;
-import javax.sql.DataSource;
 
 
 
@@ -31,25 +19,18 @@ public class ControllerDB {
 	@PostMapping("/login")
 
 	public ResponseEntity<String> ejemplo(@RequestBody String mensaje) {
-	//public void recibirMensaje(@RequestBody String mensaje) {
         System.out.println("Mensaje recibido desde el cliente: " + mensaje);
         // Aquí puedes hacer lo que quieras con el mensaje recibido
         
         JSONObject jsonObject = new JSONObject(mensaje);
         String username = jsonObject.getString("username_db");
-        String roles = "Estandar";
+        String rol = "Estandar";
         String email = jsonObject.getString("email_db");
         String user_password = jsonObject.getString("user_password_db");
         String phone = jsonObject.getString("phone_db");
         String address = jsonObject.getString("address_db");
         
-        
-        String nombre="john";
-        
-        String respuesta = "0";
-        //System.out.println(jsonObject.getClass().getName());
-        //System.out.println(valor.getClass().getName());
-        
+        String response = "0";
         
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         
@@ -60,9 +41,9 @@ public class ControllerDB {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         
         String sql = "INSERT INTO Users (username, roles, email, user_password, phone, address) VALUES (?, ?, ?,?,?,?)";
-        jdbcTemplate.update(sql, username, roles, email, user_password, phone, address);
+        jdbcTemplate.update(sql, username, rol, email, user_password, phone, address);
         
-        return ResponseEntity.ok(respuesta);
+        return ResponseEntity.ok(response);
         
         
 
