2a62967a2c4165b59717894162b81986fa11ee9c
Guillem Albo Pintor <albopintor@gmail.com>
2023-04-18 15:23:47 +0200

    Update

--- WoofFinder-Frontend/vite.config.js
+++ WoofFinder-Frontend/vite.config.js
@@ -5,12 +5,4 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        home: resolve(__dirname, "home.html"),
-      },
-    },
-  },
 });
