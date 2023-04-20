50ad34a85ccf32ae7bb647053cca4b7147f1dc92
AndyRoca <andres.rivera.geo@gmail.com>
2023-04-20 19:53:59 +0200

    Frontend advances: slider and header

--- WoofFinder-Frontend/src/components/SlideShow.jsx
+++ WoofFinder-Frontend/src/components/SlideShow.jsx
@@ -1,30 +1,57 @@
 import React from 'react'
+import { useState } from 'react'
+import "../assets/styles/App.css"
+import headerPicture1 from '../assets/styles/sources/headerPicture1.jpg';
+import headerPicture2 from '../assets/styles/sources/headerPicture2.jpg';
+import headerPicture3 from '../assets/styles/sources/headerPicture3.jpg';
+
 
 export default function SlideShow() {
-
-  return (
-    <div>
-      
-    </div>
-  )
-
   let sliderData = [
+    
     {
-      image:<img src="src\assets\styles\sources\headerPicture1.jpg"/>,
+      image: headerPicture1,
       heading:"Woof Finder",
       desc:"Tu mascota esta esperando por ti...Encuentra el mejor lugar para adoptar."
     },
 
     {
-      image:<img src="src\assets\styles\sources\headerPicture2.jpg"/>,
+      image:headerPicture2,
       heading:"Woof Finder",
       desc:"Tu mascota esta esperando por ti...Encuentra el mejor lugar para adoptar."
     },
 
     {
-      image:<img src="src\assets\styles\sources\headerPicture3.jpg"/>,
+      image:headerPicture3,
       heading:"Woof Finder",
       desc:"Tu mascota esta esperando por ti...Encuentra el mejor lugar para adoptar."
     }
   ]
-}
+
+  const [currentSlide, setcurrentSlide] = useState(0)
+
+  return (
+    <div className='slider'>
+      
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+      <i className="fa-solid fa-angle-left"></i>
+      <i className="fa-solid fa-angle-right"></i>
+      
+      {sliderData.map((slide, index) => {
+        return(
+          <div className={index === currentSlide ? "slide current" : slide} key ={index}>
+            {index === currentSlide && (
+              <>
+              <img src={slide.image} alt="slide"/>
+              <div className="content">
+                <h1 className='heading'>{slide.heading}</h1>
+                <p className='description'>{slide.desc}</p>
+                <button className="--btn--btn--primary">Ger Started</button>
+              </div>
+              </>
+            )}
+          </div>
+        )
+      })}
+    </div>
+  )}
