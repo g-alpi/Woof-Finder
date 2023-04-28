1c16cb2e35a5d1a3f65a6d43c794ce8455fff3a5
AndyRoca <andres.rivera.geo@gmail.com>
2023-04-28 18:51:29 +0200

    Fisnish petpopup and change the visuals from adopt

--- WoofFinder-Frontend/src/pages/Adopt/PetPopUp.jsx
+++ WoofFinder-Frontend/src/pages/Adopt/PetPopUp.jsx
@@ -17,7 +17,7 @@ export default function PetPopUp({ selectedPet, toggleFlag }) {
   return (
     <>
       <div className="popUpBackground" onClick={toggleFlag}></div>
-      <div className="popUp ">
+      <div className="popUp petPopUp">
         <i class="fa-solid fa-xmark" onClick={toggleFlag}></i>
         <div className="pet">
           <img src={img} alt={pet_name} />
@@ -36,8 +36,7 @@ export default function PetPopUp({ selectedPet, toggleFlag }) {
 
             <div>
               <div className="description">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                {/* <p>{pet_description}</p> */}
+                <p>{pet_description}</p>
               </div>
               <button className="btnPrimary">Adoptar</button>
             </div>
