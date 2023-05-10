49dac467f5680f8295cdae28f41abd52b06d29f9
Andres Rivera Castaño <andres.rivera.geo@gmail.com>
2023-05-10 16:30:24 +0200

    fix the bug that let create a new pet without log in.

--- WoofFinder-Frontend/src/components/InsertPet.jsx
+++ WoofFinder-Frontend/src/components/InsertPet.jsx
@@ -6,8 +6,29 @@ export default function InsertPet() {
   const [insertFlag, setInsertFlag] = useState(false);
 
   const toggleInsertFlag = () => {
-    insertFlag === true ? setInsertFlag(false) : setInsertFlag(true);
+    
+    if (localStorage.length > 0) {
+      insertFlag ? setInsertFlag(false) : setInsertFlag(true);
+    } else {
+      let div = document.createElement("div");
+      div.classList.add("alert");
+      div.append("¡Es necesario iniciar sesión!");
+
+      let link = document.createElement("a");
+      link.setAttribute("href", "/Login");
+      link.innerText = "Iniciar sesión";
+
+      div.appendChild(document.createElement("br"));
+      div.appendChild(link);
+
+      document.querySelector("body").append(div);
+
+      setTimeout(() => {
+        document.querySelector(".alert").remove();
+      }, 4000);
+    }
   };
+
   return (
     <>
       {insertFlag && <InsertPopUp toggleFlag={toggleInsertFlag} />}
